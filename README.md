9b48815958612cd4c1fbc5519da7a26b6fd1f2f9
Nava Teja <mushamnavam9530@gmail.com>
2024-03-03 12:38:59 +0530

    Updated according to PEP8

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Navam's Number (NN)
 
 
-> Any Number which can be expressed as a sum of multiple numbers called contributors raised to some power, in multiple ways is called as a Navam's Number!
+> Any Number which can be expressed as a sum of multiple numbers (called contributors) raised to some power, in multiple ways is called as a Navam's Number!
 
 To understand the concept better, consider the Ramanujan's Number as an example:
 
@@ -9,19 +9,20 @@ $1729 = (1)^3 + (12)^3 = (9)^3 + (10)^3$
 
 The Significance of the Number 1729 is that it is the smallest number which can be expressed as a sum of 2 cubes in 2 different ways. Hence, 1729 (Ramanujan's Number) is also a Navam's Number.
 
-## Features of Navam's Number
+## Atrributes of Navam's Number
 ### Contributors
 * The numbers which are required to be raised to their powers and added to get a Navam's Number are called as the Contributors of that Navam's Number.
 * For example, numbers 1, 9, 10, 12 are called the Contributors of the Ramanujan's Number.
+* Remember, the Contributor must be a Natural Number!
 ### Exponent
 * The Power of the Contributors is called as the Exponent.
 * For example, number 3 is the exponent for the Ramanujan's Number.
 * Remember, the Exponent must be a Whole Number!
-### Terms
-* The minimum number of Contributors required to form a Navam's Number is known as the Number of Terms.
+### Term Count
+* The minimum number of Contributors required to form a Navam's Number is known as the Term Count.
 * For example, numbers 1 and 12 are enough to form Ramanujan's Number.
 * So, the Number of Terms in Ramanujan's Number is 2.
-* Remember, the Number of Terms must be a Natural Number!
+* Remember, the Term Count must be a Natural Number!
 ### Parity
 * The number of ways in which a Navam's Number can be expressed is called as the Parity of that Navam's Number.
 * For example, 1729 can be expressed in 2 ways.
@@ -29,8 +30,6 @@ The Significance of the Number 1729 is that it is the smallest number which can 
 * Remember, the Parity must be greater than 1!
 
 ## Note
-* The code in Find_NN.py finds the Navam's Numbers in a range $[minimum, maximum]$ where:
-    * $minimum$ is the Lowest Number and $maximum$ is the Highest Number to be the Contributors.
-    * Both $minimum$ and $maximum$ are inclusive in the Range.
-
-* Make sure to install the Pandas library before executing the file!
+The code in _main.py_ finds the Navam's Numbers in a range $[minimum, maximum]$ where:
+* $minimum$ is the Lowest Number and $maximum$ is the Highest Number to be the Contributors.
+* Both $minimum$ and $maximum$ are inclusive in the Range.
--- main.py
+++ main.py
@@ -1,87 +1,89 @@
-# Downloading the Required Packages
-import os, platform
-os.system('pip install pandas')
+"""This module returns the Navam's Numbers with provided attributes"""
 
-# Importing the Necessary Libraries
-import pandas as pd
 from itertools import combinations
+import pandas as pd
 
-# Defining Functions to check the validity of Features of Navam's Number
-def check_minmax(min, max):
-    if not max > min:
+def check_minmax(minimum: int, maximum: int) -> tuple:
+    """Checks the validity of the range of the Contributors"""
+
+    if minimum >= maximum:
         print('Maximum Number should be Greater than Minimum Number!')
-        min = int(input('Enter the Valid Minimum Number: '))
-        max = int(input('Enter the Valid Maximum Number: '))
-        return check_minmax(min, max)
-    else:
-        return min, max
-    
-def check_expo(expo):
-    if expo < 0:
+        minimum = int(input('Enter the Valid Minimum Number: '))
+        maximum = int(input('Enter the Valid Maximum Number: '))
+        return check_minmax(minimum, maximum)
+    return minimum, maximum
+
+def check_expo(exponent: int) -> int:
+    """Checks if the specified Exponent is valid"""
+
+    if exponent < 0:
         print('Exponent must be a Whole Number!')
-        expo = int(input('Enter the Valid Exponent: '))
-        return check_expo(expo)
-    else:
-        return expo
+        exponent = int(input('Enter the Valid Exponent: '))
+        return check_expo(exponent)
+    return exponent
 
-def check_term(term):
-    if term < 1:
+def check_term(term_count: int) -> int:
+    """Checks if the specified Term Count is valid"""
+
+    if term_count < 1:
         print('Number of Terms must be a Natural Number!')
-        term = int(input('Enter the Valid Number of Terms: '))
-        return check_term(term)
-    else:
-        return term
+        term_count = int(input('Enter the Valid Number of Terms: '))
+        return check_term(term_count)
+    return term_count
 
-def check_parity(parity):
+def check_parity(parity: int) -> int:
+    """Checks if the specified Parity is valid"""
+
     if parity < 2:
         print('Parity must be Greater than 1!')
         parity = int(input('Enter the Valid Parity: '))
         return check_parity(parity)
+    return parity
+
+def logic():
+    """Prints Navam's Numbers with provided attributes"""
+
+    mini = int(input('Enter the Minimum Number: '))
+    maxi = int(input('Enter the Maximum Number: '))
+    mini, maxi = check_minmax(mini, maxi)
+    expo = int(input('Enter the Exponent: '))
+    expo = check_expo(expo)
+    term = int(input('Enter the Term Count: '))
+    term = check_term(term)
+    pari = int(input('Enter the Parity: '))
+    pari = check_parity(pari)
+
+    # Creating all the Possible Combinations with the numbers in the range of Contributors
+    numbers_list = []
+    contributors_list = []
+    for contributors in combinations(range(mini, maxi + 1), term):
+        contributors_list.append(contributors)
+        number = 0
+        for contributor in contributors:
+            number += contributor ** expo
+        numbers_list.append(number)
+
+    # Extracting the Navam's Numbers from the Possible Combinations
+    navams_numbers_list = []
+    navams_numbers_contributors_list = []
+    for index, number in enumerate(numbers_list):
+        if numbers_list.count(number) >= pari:
+            navams_numbers_list.append(number)
+            navams_numbers_contributors_list.append(contributors_list[index])
+
+    # Filtering out the non-duplicate Navam's Numbers along with their Contributors
+    if navams_numbers_list:
+        navams_numbers = {}
+        for index, navams_number in enumerate(navams_numbers_list):
+            if navams_number not in navams_numbers:
+                navams_numbers[navams_number] = [navams_numbers_contributors_list[index]]
+            elif len(navams_numbers[navams_number]) != pari:
+                navams_numbers[navams_number].append(navams_numbers_contributors_list[index])
+        df = pd.DataFrame(navams_numbers)
+        print("\nThe Navam's Numbers are:")
+        print(df.T.sort_index().to_string(header=False))
     else:
-        return parity
+        print('There are no Navam\'s Numbers in the given Range of Contributors!')
 
-# Taking the Features of Navam's Numbers as inputs from the user
-if platform.system() == 'Windows':
-    os.system('cls')
-else:
-    os.system('clear')
-min = int(input('\n\n\n\nEnter the Minimum Number: '))
-max = int(input('Enter the Maximum Number: '))
-min, max = check_minmax(min, max)
-expo = int(input('Enter the Exponent: '))
-expo = check_expo(expo)
-term = int(input('Enter the Number of Terms: '))
-term = check_term(term)
-parity = int(input('Enter the Parity: '))
-parity = check_parity(parity)
-
-# Creating all the Possible Combinations with the numbers in the range of Contributors
-numbers_list = []
-contributors_list = []
-for contributors in combinations(range(min, max + 1), term):
-    contributors_list.append(contributors)
-    number = 0
-    for contributor in contributors:
-        number += contributor ** expo
-    numbers_list.append(number)
-
-# Extracting the Navam's Numbers from the Possible Combinations
-navams_numbers_list = []
-navams_numbers_contributors_list = []
-for index, number in enumerate(numbers_list):
-    if numbers_list.count(number) >= parity:
-        navams_numbers_list.append(number)
-        navams_numbers_contributors_list.append(contributors_list[index])
-
-# Filtering out the non-duplicate Navam's Numbers along with their Contributors
-if navams_numbers_list:
-    navams_numbers = {}
-    for index, navams_number in enumerate(navams_numbers_list):
-        if navams_number not in navams_numbers:
-            navams_numbers[navams_number] = [navams_numbers_contributors_list[index]]
-        elif len(navams_numbers[navams_number]) != parity:
-            navams_numbers[navams_number].append(navams_numbers_contributors_list[index])
-    df = pd.DataFrame(navams_numbers)
-    print(df.T.sort_index().to_string(header=False))
-else:
-    print('There are no Navam\'s Numbers in the given Range of Contributors!')
+if __name__ == '__main__':
+    logic()
